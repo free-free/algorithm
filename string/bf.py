2acb84a97a381b50a6c138faf4c39d58738c5b7e
jell <infinite.ft@gmail.com>
2019-04-24 10:22:33 +0800

    Make bf() function support multiple position finding

--- string/bf.py
+++ string/bf.py
@@ -1,19 +1,22 @@
 # -*- coding:utf-8 -*-
 
-def bf(string, pattern_s):
+def bf(string, pattern_s, multi=False):
     n, m = len(string), len(pattern_s)
     if n < m:
         return None
+    idxs = []
     for i in range(n - m + 1):
         if string[i : i + m] == pattern_s:
-            return i
-    return None
+            idxs.append(i)
+            if not multi:
+                break
+    return idxs
 
 
 
 if __name__ == '__main__':
-    string = 'hello world! this is new life'
+    string = 'hello world! this is new life new life'
     pattern_s = 'new'
     print(string)
     print(pattern_s)
-    print(bf(string,pattern_s))
+    print(bf(string, pattern_s, True))
